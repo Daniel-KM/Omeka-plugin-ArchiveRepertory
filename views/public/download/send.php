127377133275ae0be46156a2d940e41a3f437b21
Daniel Berthereau <Daniel.github@Berthereau.net>
2014-04-08 02:46:02 +0200

    Added a javascript timer to download files.

--- views/public/download/send.php
+++ views/public/download/send.php
@@ -16,10 +16,20 @@
     <div id='send-download'>
         <?php echo flash(); ?>
         <h2><?php echo __('Downloading file'); ?></h2>
-        <p><?php echo __('Your download should start automatically in five seconds. If not, click %shere%s.', '<a href="' . $sendUrl . '">', '</a>'); ?>
+        <p><?php echo __('Your download should start automatically in %s5%s seconds. If not, click %shere%s.', '<span id="download_timer">', '</span>', '<a href="' . $sendUrl . '">', '</a>'); ?>
         <p><?php echo __('Go back to the %sprevious page%s.', '<a href="' . $redirect . '">', '</a>'); ?></p>
     </div>
 </div>
+<script type="text/javascript">
+    var seconds_left = 5;
+    var interval = setInterval(function() {
+        document.getElementById('download_timer').innerHTML = --seconds_left;
+        if (seconds_left <= 0) {
+            document.getElementById('download_timer').innerHTML = '0';
+            clearInterval(interval);
+        }
+    }, 1000);
+</script>
 <?php
     echo foot();
 ?>
