dc2aca6f578d60f1f4fb5015490fb9505088131a
Daniel Berthereau <Daniel.github@Berthereau.net>
2014-08-11 02:47:02 +0200

    Cleaned anti-download controller.

--- controllers/DownloadController.php
+++ controllers/DownloadController.php
@@ -59,7 +59,7 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
         }
 
         // File is good.
-        if ($this->_toConfirm) {
+        if ($this->_getToConfirm()) {
             // Filepath is not saved in session for security reason.
             $this->session->filename = $this->_filename;
             $this->session->type = $this->_type;
@@ -82,11 +82,10 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
             return $this->_gotoSourcePage();
         }
 
-        $this->view->form = $this->_getConfirmForm();
+        $form = $this->_getConfirmForm();
+        $this->view->form = $form;
         $this->view->filesize = $this->_formatFileSize($this->_getFilesize());
         $this->view->source_page = $this->session->sourcePage;
-        // Kept temporary to avoid change of theme.
-        $this->view->redirect = $this->session->sourcePage;
 
         if (!$this->getRequest()->isPost()) {
             return;
@@ -110,14 +109,12 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
     function sendAction()
     {
         if (!$this->_checkSession()) {
-            $this->_helper->flashMessenger(__('Download error.'), 'error');
+            $this->_helper->flashMessenger(__('Download error: File already sent.'), 'error');
             return $this->_gotoSourcePage();
         }
 
         $this->view->sendUrl = WEB_ROOT . '/archive-repertory/download/send';
         $this->view->source_page = $this->session->sourcePage;
-        // Kept temporary to avoid change of theme.
-        $this->view->redirect = $this->session->sourcePage;
 
         if (!isset($this->session->checked)) {
             $this->session->checked = true;
@@ -134,16 +131,16 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
     protected function _sendFile()
     {
         // Everything has been checked.
-        $filepath = $this->_filepath;
+        $filepath = $this->_getFilepath();
         $filesize = $this->_getFilesize();
-        $file = $this->_file;
+        $file = $this->_getFile();
         $contentType = $this->_getContentType();
-        $mode = $this->_mode;
+        $mode = $this->_getMode();
 
         // Save the stats if the plugin Stats is ready.
         if (plugin_is_active('Stats') && $this->_getTheme() == 'public') {
-            $type = $this->_type;
-            $filename = $this->_filename;
+            $type = $this->_getType();
+            $filename = $this->_getFilename();
             $this->view->stats()->new_hit(
                 // The redirect to is not useful, so keep original url.
                 '/files/' . $type . '/' . $filename,
@@ -168,54 +165,33 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
      */
     protected function _checkPost()
     {
-        // Check storage type.
-        $type = $this->getRequest()->getParam('type');
-        $storage = $this->_checkStorageType($type);
-        if (empty($storage)) {
+        if (!$this->_getStorage()) {
             return false;
         }
 
-        // Check filename and secure filepath.
-        $filename = $this->getRequest()->getParam('filename');
-        $filepath = $this->_checkFilename($filename);
-        if (empty($filepath)) {
+        if (!$this->_getFilename()) {
             return false;
         }
 
-        // Check if the file exists in the base.
-        // In all case, we redo search from base to avoid session change.
-        $file = $this->_file = $this->_getFileFromFilename($filename);
-        if (empty($file)) {
+        if (!$this->_getFilepath()) {
             return false;
         }
 
-        // Check if the file belongs to a public item.
-        $item = get_record_by_id('Item', $file->item_id);
-        if (empty($item)) {
+        if (!$this->_getFilesize()) {
             return false;
         }
 
-        // Check if a confirmation is needed.
-        $confirm = $this->_checkConfirmation();
-
-        // Check mode of disposition.
-        if ($confirm) {
-            $mode = $this->_mode = 'attachment';
-        }
-        else {
-            $mode = $this->getRequest()->getParam('mode');
-            $mode = $this->_checkDisposition($mode);
-            if (empty($mode)) {
-                return false;
-            }
+        if (!$this->_getFile()) {
+            return false;
         }
 
-        // Update session if needed.
-        if (empty($this->session->sourcePage)) {
-            // TODO Redirect to files/show page? Add an option.
-            $this->_sourcePage = record_url($item, null, true);
-            $this->session->sourcePage = $this->_sourcePage;
+        if (!$this->_getContentType()) {
+            return false;
         }
+
+        if (!$this->_getMode()) {
+            return false;
+        };
 
         return true;
     }
@@ -250,92 +226,133 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
             }
         }
 
-        // Check storage type.
-        // In case of a confirmation, there are some params.
-        $storage = $this->_checkStorageType($this->_type);
-        if (empty($storage)) {
+        if (!$this->_getStorage()) {
             return false;
         }
 
-        // Check filename and secure filepath.
-        $filename = $this->_filename;
-        $filepath = $this->_checkFilename($filename);
-        if (empty($filepath)) {
-                return false;
+        if (!$this->_getFilename()) {
+            return false;
         }
 
-        // Check if the file exists in the base.
-        // In all case, we redo search from base to avoid session change.
-        $file = $this->_file = $this->_getFileFromFilename($filename);
-        if (empty($file)) {
-                return false;
+        if (!$this->_getFilepath()) {
+            return false;
         }
 
-        // Check if the file belongs to a public item.
-        $item = get_record_by_id('Item', $file->item_id);
-        if (empty($item)) {
-                return false;
+        if (!$this->_getFilesize()) {
+            return false;
         }
 
-        // Check mode of disposition.
+        if (!$this->_getFile()) {
+            return false;
+        }
+
+        if (!$this->_getContentType()) {
+            return false;
+        }
+
         // If we come back here, this is a confirmation and mode is attachment.
-        $mode = $this->_mode = 'attachment';
+        $this->_mode = 'attachment';
 
         return true;
     }
 
     /**
-     * Check type of storage.
+     * Get and set type (generally original, sometimes fullsize).
      *
-     * @return string Path to the storage of the selected type of file. Empty
-     * if incorrect.
+     * @internal The type is not checked, but if not authorized, storage will
+     * return an error.
+     *
+     * @return string ("original" by default)
      */
-    protected function _checkStorageType($type = 'original')
+    protected function _getType()
     {
-        if (empty($type)) {
-            $type = 'original';
-        }
-        $this->_type = $type;
+        if (is_null($this->_type)) {
+            $this->_type = $this->_request->getParam('type');
 
-        // This is used to get list of storage path. Is there a better way?
-        // getPathByType() is not secure.
-
-        // For hacked core (before Omeka 2.2).
-        $file = new File;
-        if (method_exists($file, 'getStoragePathsByType')) {
-            $storagePaths = $file->getStoragePathsByType();
-            if (!in_array($type, $storagePaths)) {
-                return false;
+            // Default type.
+            if (empty($this->_type)) {
+                $this->_type = 'original';
             }
-            $this->_storage = $storagePaths[$type];
         }
-        // Before Omeka 2.2.
-        else {
-            $storagePath = $file->getStoragePath($type);
-            if ($type == 'original') {
-                $this->_storage = substr($storagePath, 0, strlen($storagePath) - 1);
+
+        return $this->_type;
+    }
+
+    /**
+     * Get, check and set type of storage.
+     *
+     * @return string Path to the storage of the selected type of file.
+     */
+    protected function _getStorage()
+    {
+        if (is_null($this->_storage)) {
+            $type = $this->_getType();
+
+            // This is used to get list of storage path. Is there a better way?
+            // getPathByType() is not secure.
+
+            // For hacked core (before Omeka 2.2).
+            $file = new File;
+            if (method_exists($file, 'getStoragePathsByType')) {
+                $storagePaths = $file->getStoragePathsByType();
+                if (!in_array($type, $storagePaths)) {
+                    $this->_storage = false;
+                    return false;
+                }
+                $this->_storage = $storagePaths[$type];
             }
+            // Omeka 2.2.
             else {
-                $this->_storage = substr($storagePath, 0, strlen($storagePath) - strlen(File::DERIVATIVE_EXT) - 2);
+                try {
+                    $storagePath = $file->getStoragePath($type);
+                } catch (RuntimeException $e) {
+                    $this->_storage = false;
+                    return false;
+                }
+                $this->_storage = ($type == 'original')
+                    ? substr($storagePath, 0, strlen($storagePath) - 1)
+                    : substr($storagePath, 0, strlen($storagePath) - strlen(File::DERIVATIVE_EXT) - 2);
             }
         }
+
         return $this->_storage;
     }
 
     /**
-     * Check filepath.
+     * Get and set filename.
      *
-     * @return string Path to the file. Empty if error.
+     * @internal The filename is not checked, but if not existing, filepath will
+     * return an error.
+     *
+     * @return string Filename.
      */
-    protected function _checkFilename($filename)
+    protected function _getFilename()
     {
-        $storagePath = FILES_DIR . DIRECTORY_SEPARATOR . $this->_storage . DIRECTORY_SEPARATOR;
-        $filepath = realpath($storagePath . $filename);
-        if (strpos($filepath, $storagePath) !== 0) {
-            return false;
+        if (is_null($this->_filename)) {
+            $this->_filename = $this->_request->getParam('filename');
         }
-        $this->_filename = $filename;
-        $this->_filepath = $filepath;
+
+        return $this->_filename;
+    }
+
+    /**
+     * Get and set filepath.
+     *
+     * @return string Path to the file.
+     */
+    protected function _getFilepath()
+    {
+        if (is_null($this->_filepath)) {
+            $filename = $this->_getFilename();
+            $storage = $this->_getStorage();
+            $storagePath = FILES_DIR . DIRECTORY_SEPARATOR . $this->_storage . DIRECTORY_SEPARATOR;
+            $filepath = realpath($storagePath . $filename);
+            if (strpos($filepath, $storagePath) !== 0) {
+                return false;
+            }
+            $this->_filepath = $filepath;
+        }
+
         return $this->_filepath;
     }
 
@@ -347,7 +364,7 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
     protected function _getFilesize()
     {
         if (is_null($this->_filesize)) {
-            $filepath = $this->_filepath;
+            $filepath = $this->_getFilepath();
             $this->_filesize = @filesize($filepath);
         }
 
@@ -359,12 +376,40 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
      *
      * @return File|null
      */
+    protected function _getFile()
+    {
+        if (is_null($this->_file)) {
+            $filename = $this->_getFilename();
+            if ($this->_getStorage() == 'original') {
+                $this->_file =  get_db()->getTable('File')->findBySql('filename = ?', array($filename), true);
+            }
+           // Get a derivative: this is functional only because filenames are
+           // hashed.
+            else {
+                $originalFilename = substr($filename, 0, strlen($filename) - strlen(File::DERIVATIVE_EXT) - 1);
+                $this->_file = get_db()->getTable('File')->findBySql('filename LIKE ?', array($originalFilename . '%'), true);
+            }
+
+            // Check rights: if the file belongs to a public item.
+            if (empty($this->_file) || empty($this->_file->getItem())) {
+                $this->_file = false;
+            }
+        }
+
+        return $this->_file;
+     }
+
+    /**
+     * Set and get file object from the filename. Rights access are checked.
+     *
+     * @return File|null
+     */
     protected function _getContentType()
     {
         if (is_null($this->_contentType)) {
-            $type = $this->_type;
+            $type = $this->_getType();
             if ($type == 'original') {
-                $file = $this->_file;
+                $file = $this->_getFile();
                 $this->_contentType = $file->mime_type;
             }
             else {
@@ -376,72 +421,80 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
     }
 
     /**
-     * Check rights to direct download.
+     * Get and set rights to direct download.
      *
      * @return boolean False if confirmation is not needed, else true.
      */
-    protected function _checkConfirmation()
+    protected function _getToConfirm()
     {
-        if (current_user()) {
-            $this->_toConfirm = false;
-        }
+        if (is_null($this->_toConfirm)) {
+            if (current_user()) {
+                $this->_toConfirm = false;
+            }
 
-        // Check for captcha;
-        else {
-            $filesize = $this->_getFilesize();
-            $this->_toConfirm = ($filesize > (integer) get_option('archive_repertory_warning_max_size_download'));
+            // Check for captcha;
+            else {
+                $filesize = $this->_getFilesize();
+                $this->_toConfirm = ($filesize > (integer) get_option('archive_repertory_warning_max_size_download'));
+            }
         }
 
         return $this->_toConfirm;
     }
 
     /**
-     * Check sending mode.
+     * Get and set sending mode.
      *
      * @return string Disposition 'inline' (default) or 'attachment'.
      */
-    protected function _checkDisposition($mode)
+    protected function _getMode()
     {
-        $filepath = &$this->_filepath;
-        $file = &$this->_file;
-        $disposition = &$this->_mode;
+        if (is_null($this->_mode)) {
+            if ($this->_getToConfirm()) {
+                $this->_mode = 'attachment';
+                return $this->_mode;
+            }
 
-        // Prepare headers.
-        switch ($mode) {
-            case 'stream':
-            case 'inline':
-                $disposition = 'inline';
-                break;
+            // Prepare headers.
+            $mode = $this->_request->getParam('mode', 'inline');
+            switch ($mode) {
+                case 'inline':
+                    $this->_mode = 'inline';
+                    break;
 
-            case 'download':
-            case 'attachment':
-                $disposition = 'attachment';
-                break;
+                case 'attachment':
+                    $this->_mode = 'attachment';
+                    break;
 
-            case 'size':
-                $disposition = ($file->size > (integer) get_option('archive_repertory_warning_max_size_download'))
-                    ? 'attachment'
-                    : 'inline';
-                break;
+                case 'size':
+                    $filesize = $this->_getFilesize();
+                    $this->_mode = ($filesize > (integer) get_option('archive_repertory_warning_max_size_download'))
+                        ? 'attachment'
+                        : 'inline';
+                    break;
 
-            case 'image':
-                $disposition = (strpos($file->mime_type, 'image') === false)
-                    ? 'attachment'
-                    : 'inline';
-                break;
+                case 'image':
+                    $contentType = $this->_getContentType();
+                    $this->_mode = (strpos($contentType, 'image') === false)
+                        ? 'attachment'
+                        : 'inline';
+                    break;
 
-            case 'image-size':
-                $disposition = (strpos($file->mime_type, 'image') === false
-                        || $file->size > (integer) get_option('archive_repertory_warning_max_size_download'))
-                    ? 'attachment'
-                    : 'inline';
-                break;
+                case 'image-size':
+                    $filesize = $this->_getFilesize();
+                    $contentType = $this->_getContentType();
+                    $this->_mode = (strpos($contentType, 'image') === false
+                            || $filesize > (integer) get_option('archive_repertory_warning_max_size_download'))
+                        ? 'attachment'
+                        : 'inline';
+                    break;
 
-            default:
-                $disposition = 'inline';
+                default:
+                    $this->_mode = 'inline';
+            }
         }
 
-        return $disposition;
+        return $this->_mode;
     }
 
     /**
@@ -464,17 +517,6 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
         }
 
         return $this->_theme;
-    }
-
-    /**
-     * Get the captcha form.
-     *
-     * @return ArchiveRepertory_ConfirmForm
-     */
-    protected function _getConfirmForm()
-    {
-        require_once PLUGIN_DIR . '/ArchiveRepertory/forms/ConfirmForm.php';
-        return new ArchiveRepertory_ConfirmForm();
     }
 
     /**
@@ -510,6 +552,17 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
     }
 
     /**
+     * Get the captcha form.
+     *
+     * @return ArchiveRepertory_ConfirmForm
+     */
+    protected function _getConfirmForm()
+    {
+        require_once PLUGIN_DIR . '/ArchiveRepertory/forms/ConfirmForm.php';
+        return new ArchiveRepertory_ConfirmForm();
+    }
+
+    /**
      * Return a file size with the appropriate format of unit.
      *
      * @return string
@@ -532,12 +585,4 @@ class ArchiveRepertory_DownloadController extends Omeka_Controller_AbstractActio
             }
         }
     }
-
-    /**
-     * Allow to get file from a filename.
-     */
-    protected function _getFileFromFilename($filename)
-    {
-        return get_db()->getTable('File')->findBySql('filename = ?', array($filename), true);
-     }
 }
