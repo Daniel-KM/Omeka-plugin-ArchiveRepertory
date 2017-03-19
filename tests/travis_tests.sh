8ba2d60f0ff8435d41ed92001adb9decf6e143a3
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-03-20 00:00:00 +0100

    Added Travis tests.

---
+++ tests/travis_tests.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+if [ -z $PLUGIN_DIR ]; then
+  PLUGIN_DIR=`pwd`
+fi
+
+if [ -z $OMEKA_DIR ]; then
+  export OMEKA_DIR=`pwd`/omeka
+  echo "omeka_dir set"
+fi
+
+echo "Plugin Directory: $PLUGIN_DIR"
+echo "Omeka Directory: $OMEKA_DIR"
+
+cat $(which phpunit)
+cd tests/ && phpunit --configuration phpunit.xml
